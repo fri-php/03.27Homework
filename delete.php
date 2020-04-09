d29839d6513215b387c4c5f33a2f3162e9c9627a
alexliu0121 <45057088+alexliu0121@users.noreply.github.com>
2020-04-09 23:34:12 +0800

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET["Id"];  
-if ( isset($_COOKIE[$id]) ) { 
-   
-   while ( list($name, $value) = each($_COOKIE[$id]) )
-      setcookie($id."[".$name."]", "", time()-3600); 
-}
-header("Location: shoppingcart.php");  
-?>
